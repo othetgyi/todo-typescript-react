51463c44bb94c790127550dda83240a0632918e9
Olivia Thetgyi <othetgyi@gmail.com>
2021-08-09 14:53:58 +0100

    Create todo item

---
+++ src/TodoListItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+
+interface Props {
+  todo: Todo;
+}
+export const TodoListItem: React.FC<Props> = (props) => {
+  return <li>content</li>;
+};
